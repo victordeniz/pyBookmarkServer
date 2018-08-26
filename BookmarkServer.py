b611c805d1d82f15a95ccae6f4edacf03932c9fc
Víctor Déniz Falcón <victor@moodle.com>
2018-08-26 21:23:54 +0100

    Threaded server. Class created.

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -9,6 +9,9 @@ import threading
 from socketserver import ThreadingMixIn
 from urllib.parse import unquote, parse_qs
 
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
+
 memory = {}
 
 form = '''<!DOCTYPE html>
