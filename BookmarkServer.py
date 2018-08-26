e49fe8a4b3eb2b07c1f3c69c86a85dc57e0767c9
Víctor Déniz Falcón <victor@moodle.com>
2018-08-26 21:21:28 +0100

    Threaded server

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -5,6 +5,8 @@
 import http.server
 import requests
 import os
+import threading
+from socketserver import ThreadingMixIn
 from urllib.parse import unquote, parse_qs
 
 memory = {}
@@ -109,5 +111,5 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000)) # Use PORT if it's there
     server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
